eaae3f95b128c8e31af5c64aa2628b71e7244abf
Argyris36 <61797488+Argyris36@users.noreply.github.com>
2023-09-30 15:31:41 +0100

    worflowr test

---
+++ workflow_test.R
@@ -0,0 +1,15 @@
+library("workflowr")
+wflow_start("~/argyris_code", existing = TRUE)
+wflow_build()
+wflow_view()
+wflow_status()
+wflow_publish(c("analysis/index.Rmd", "analysis/about.Rmd", "analysis/license.Rmd"),
+              "Publish the initial files for myproject")
+wflow_use_github("Argyris36")
+wflow_git_push(dry_run = TRUE)
+wflow_open("analysis/first-analysis.Rmd")
+wflow_build()
+wflow_status()
+wflow_publish(c("analysis/index.Rmd", "analysis/first-analysis.Rmd"),
+              "Add my first analysis")
+wflow_build()
