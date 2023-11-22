667a5af69a0e9cde47167b86d5fcd8d138aaab82
Argyris36 <61797488+Argyris36@users.noreply.github.com>
2023-11-22 07:11:25 +0000

    minor changes

---
+++ keyword_search_in_pdf.R
@@ -0,0 +1,46 @@
+
+# search keywords and patterns in pdf -------------------------------------
+
+
+
+library(pdfsearch)
+
+
+#### create a function
+
+pdf_search_func <- function(keyword, file ){
+  
+  result <- list()
+  
+  for(i in 1: length(file)) { 
+    
+    file <- file
+    result[[i]] <- keyword_search(file[[i]], 
+                                  keyword = c(keyword),
+                                  path = TRUE)
+  }
+  return(result)
+}
+
+
+
+
+
+
+
+#now using the function to extract all pdf files in my current directory
+library("rebus")
+
+#for example to get the following from my current folder
+files <- list.files(path="/Users/stringarisa/argyris_code/", pattern= ".pdf" %R% END , all.files=TRUE, 
+                    full.names=TRUE)
+
+
+files <- list.files(path="/Users/stringarisa/Desktop", pattern= ".pdf" %R% END , all.files=TRUE, 
+                    full.names=TRUE)
+
+#for these keywords
+keywords <- c("social", "three", "treatment")
+keywords <- ("Keren\\w\\s.\\nStringaris")
+
+pdf_search_func(keywords,files )
